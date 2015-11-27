26151d1ce8d86334e0ef810243aa78526461a786
Spencer Scorcelletti <spencerscorcelletti@gmail.com>
2015-11-27 01:59:22 -0800

    Modify prototype to work with full bootstrap.css. 146,082 bytes -> 2,225 bytes!!!

--- index.js
+++ index.js
@@ -3,26 +3,40 @@ import css from 'css'
 import fs from 'fs';
 import cheerio from 'cheerio';
 
-let html = fs.readFileSync('index.html');
+let html = fs.readFileSync('example/index.html');
 
 let $ = cheerio.load(html),
-  ast = parse(fs.readFileSync('example.css').toString());
-
-// console.log(JSON.stringify(ast));
+  ast = parse(fs.readFileSync('example/bootstrap.css').toString());
 
 let candidateRules = ast.stylesheet.rules;
 
 let activeRules = candidateRules.filter((rule) => {
-  var result = false;
-  rule.selectors.forEach((selector) => {
-    let selection = $(selector);
-    if (selection.length > 0) {
-      result = true;
-    }
-  });
-  return result;
+  switch (rule.type) {
+    case "rule":
+      var result = false;
+      rule.selectors.forEach((selector) => {
+        selector = selector.replace( /(\(|\)|=|\"|@|:|\.|\[|\]|,)/g, '\\$1' ); //" <-- atom gets confused
+        let selection = $(selector);
+        if (selection.length > 0) {
+          result = true;
+        }
+      });
+      return result;
+      break;
+    /* TODO: these cases could be useful to consider, but I'll handle them later */
+    case "comment":
+      break;
+    case "media":
+      break;
+    case "keyframes":
+      break;
+    case "font-face":
+      break;
+    default:
+      console.log("Add a case for this!", rule.type);
+  }
 });
 
 ast.stylesheet.rules = activeRules;
 
-fs.writeFileSync('example.useless.css', css.stringify(ast));
+fs.writeFileSync('example/bootstrap.useless.css', css.stringify(ast));
