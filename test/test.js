d96d875bd250a4b66a0a79d41b7aa6bdcf3cf03c
Spencer Scorcelletti <asimpletune@users.noreply.github.com>
2016-06-26 16:40:39 -0600

    Add scaffolding for tests

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import mocha from "mocha"
+import { assert } from "chai"
+
+describe('placeholder', function() {
+  it('should assert true', () => {
+    assert.isTrue(true)
+  } )
+  it('should asser false', () => {
+    assert.isFalse(false)
+  } )
+});
