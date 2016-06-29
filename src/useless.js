852660edd9c43004707f6c692542873b61b6a1c7
Spencer Scorcelletti <asimpletune@users.noreply.github.com>
2016-06-29 08:49:37 -0600

    Begin refactor of library into configurable API

--- src/useless.js
+++ src/useless.js
@@ -3,50 +3,53 @@ import css from 'css'
 import fs from 'fs';
 import cheerio from 'cheerio';
 
-export default function Useless(html) {
+/**
+ * Useless(html, options) -> [css | { cssFileName: cssString, ... }]
+ * {
+ *  concat: bool [default: true],
+ *  css: string,
+ *  file: bool,
+ *  ignore: [globPattern | [globPattern]],
+ *  only: [globPattern | [globPattern]]
+ * }
+ */
+export default function Useless(html, options){}
+
+function loadLocalCss(path){}
+function loadRemoteCss(href){}
+
+function reduceCss(htmlString, cssString) {
   let $ = cheerio.load(html)
-  let links = $('link[href$=".css"]')
-  let result = []
-  Object.keys(links).forEach((key, index) => {
-    if (index < links.length) {
-      let element = links[key]
-      let href = element.attribs.href
-      let ast = convertLocalFile($, href)
-      result.push(css.stringify(ast));
-    }
-  })
-  return result
+  let ast = parse(css);
+  let candidateRules = ast.stylesheet.rules;
+  let activeRules = candidateRules.filter(filterCssRule)
+  ast.stylesheet.rules = activeRules;
+  return css.stringify(ast)
 }
 
-function convertLocalFile($, path) {
-  let ast = parse(fs.readFileSync(path).toString());
-  let candidateRules = ast.stylesheet.rules;
-  let activeRules = candidateRules.filter((rule) => {
-    switch (rule.type) {
-      case "rule":
-        var result = false;
-        rule.selectors.forEach((selector) => {
-          selector = selector.replace( /(\(|\)|=|\"|@|:|\.|\[|\]|,)/g, '\\$1' ); //" <-- atom gets confused
-          let selection = $(selector);
-          if (selection.length > 0) {
-            result = true;
-          }
-        });
-        return result;
-        break;
-      /* TODO: these cases could be useful to consider, but I'll handle them later */
-      case "comment":
-        break;
-      case "media":
-        break;
-      case "keyframes":
-        break;
-      case "font-face":
-        break;
-      default:
-        console.log("Add a case for this!", rule.type);
-    }
-  });
-  ast.stylesheet.rules = activeRules;
-  return ast
+function filterCssRule(rule) {
+  switch (rule.type) {
+    case "rule":
+      var result = false;
+      rule.selectors.forEach((selector) => {
+        selector = selector.replace( /(\(|\)|=|\"|@|:|\.|\[|\]|,)/g, '\\$1' ); //" <-- atom, :(
+        let selection = $(selector);
+        if (selection.length > 0) {
+          result = true;
+        }
+      });
+      return result;
+      break;
+    /* TODO: these cases could be useful to consider, but I'll handle them later */
+    case "comment":
+      break;
+    case "media":
+      break;
+    case "keyframes":
+      break;
+    case "font-face":
+      break;
+    default:
+      console.log("Add a case for this!", rule.type);
+  }
 }
